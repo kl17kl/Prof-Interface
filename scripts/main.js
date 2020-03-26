cf9752f687627754f884a8518aee2c1021819f11
Katie Lee <31972297+kl17kl@users.noreply.github.com>
2020-03-25 21:16:54 -0400

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -99,3 +99,18 @@ function showPopup(x) {
     var popup = document.getElementById(x);
     popup.classList.toggle("show");
   }
+
+// This function displays whats hidden behind a tab
+function openTab(evt, tab) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tab).style.display = "block";
+    evt.currentTarget.className += " active";
+}
